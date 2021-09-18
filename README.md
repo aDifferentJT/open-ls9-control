692e199436f7ef6edaca1c9c8fb7e37a9648a29f
Jonathan Tanner <>
2021-09-18 02:23:32 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# open-ls9-control
+
+Open LS9 Control is a C++ library for controlling the Yamaha LS9 mixer.
+There are also Python bindings included, see [PyLS9](#pyls9)
+
+## PyLS9
+
+PyLS9 is the python bindings for open-ls9-control.
+
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from distutils.core import setup, Extension
 
-module1 = Extension(
-    'pyls9',
-    language = 'c++',
-    define_macros = [('__MACOSX_CORE__', None), ('TARGET_OS_IPHONE', 0)],
-    extra_compile_args = ['--std=c++17'],
-    extra_link_args = ['-framework', 'CoreMIDI', '-framework', 'CoreAudio', '-framework', 'CoreFoundation'],
-    include_dirs = ['include'],
-    sources = ['src/RtMidi.cpp', 'src/python.cpp'],
-    py_limited_api = True)
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
-setup (name = 'pyls9',
-       version = '1.0',
-       description = 'A library to control the Yamaha LS9',
-       author = 'Jonathan Tanner',
-       url = 'http://github.com/nixCodeX/pyls9',
-       long_description = '''
-A library to control the Yamaha LS9
-''',
-       ext_modules = [module1])
+module1 = Extension \
+  ( 'pyls9'
+  , language = 'c++'
+  , define_macros = [('__MACOSX_CORE__', None), ('TARGET_OS_IPHONE', 0)]
+  , extra_compile_args = ['--std=c++17']
+  , extra_link_args = ['-framework', 'CoreMIDI', '-framework', 'CoreAudio', '-framework', 'CoreFoundation']
+  , include_dirs = ['include']
+  , sources = ['src/RtMidi.cpp', 'src/python.cpp']
+  , py_limited_api = True
+  )
+
+setup \
+  ( name = 'pyls9'
+  , version = '1.0.0'
+  , description = 'A library to control the Yamaha LS9'
+  , author = 'Jonathan Tanner'
+  , url = 'http://github.com/nixCodeX/pyls9'
+  , long_description = long_description
+  , long_description_content_type='text/markdown'
+  , ext_modules = [module1]
+  )
 
